0ebd7bc7fb1b12a16dcf0b1cf12cca2757307bda
Sudhir Attri <sudhir18267@iiitd.ac.in>
2021-06-07 15:32:00 +0530

    cpcbOntoFInalDone

--- scripts/csvEditor.js
+++ scripts/csvEditor.js
@@ -2,6 +2,7 @@ const log4js = require('log4js');
 const logger = log4js.getLogger("cpcb-scraper");
 const util = require('util');
 const fs = require('fs').promises;
+var path = require("path");
 const copyFile = util.promisify(require('fs').copyFile);
 const unlink = util.promisify(require('fs').unlink)
 const readFile = util.promisify(require('fs').readFile)
